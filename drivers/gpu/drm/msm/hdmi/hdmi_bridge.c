880b6d0213d1a8e312768030d38394960e81d11b
Arnaud Vrac <avrac@freebox.fr>
2022-06-27 11:02:14 +0200

    drm/msm: filter out hdmi 2.0 modes
    HDMI 2.0 support is not integrated yet

--- drivers/gpu/drm/msm/hdmi/hdmi_bridge.c
+++ drivers/gpu/drm/msm/hdmi/hdmi_bridge.c
@@ -276,6 +276,12 @@ static enum drm_mode_status msm_hdmi_bridge_mode_valid(struct drm_bridge *bridge
 	long actual, requested;
 
 	requested = 1000 * mode->clock;
+	if (requested > 340000000)
+		return MODE_CLOCK_RANGE;
+	if (mode->hdisplay > 2560)
+		return MODE_CLOCK_RANGE;
+	if (drm_mode_vrefresh(mode) > 60)
+		return MODE_CLOCK_RANGE;
 
 	/* for mdp5/apq8074, we manage our own pixel clk (as opposed to
 	 * mdp4/dtv stuff where pixel clk is assigned to mdp/encoder
